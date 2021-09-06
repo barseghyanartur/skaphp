80939323154034a3be2dc3a4af5966ef7c8deb28
Artur Barseghyan <artur.barseghyan@gmail.com>
2021-09-06 23:09:20 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# ska
+
+Sign data using symmetric-key algorithm encryption. Validate signed data and identify possible validation errors. Uses sha-(1, 224, 256, 385 and 512)/hmac for signature encryption. Custom hash algorithms are allowed. Useful shortcut functions for signing (and validating) dictionaries and URLs.
+
+## Installation
+
+```shell
+$ composer require barseghyanartur/ska
+```
+
+## Running tests
+
+```shell
+$ composer test
+```
---
+++ tests/PlaceholderTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace barseghyanartur/ska\Tests;
+
+use barseghyanartur/ska\Placeholder;
+use PHPUnit\Framework\TestCase;
+
+final class PlaceholderTest extends TestCase
+{
+    private Placeholder $placeholder;
+
+    protected function setUp(): void
+    {
+        $this->placeholder = new Placeholder('Artur Barseghyan says: ');
+    }
+
+    public function testItEchoesAValue(): void
+    {
+        self::assertSame('Artur Barseghyan says: Hello', $this->placeholder->echo('Hello'));
+    }
+}
