62f48615ce430cf0940befeeb39afc1ae2a77ac4
Artur Barseghyan <artur.barseghyan@slimmer.ai>
2021-09-07 23:30:08 +0200

    More

--- README.md
+++ README.md
@@ -6,6 +6,12 @@ Uses sha-(1, 224, 256, 385 and 512)/hmac for signature encryption.
 Custom hash algorithms are allowed. Useful shortcut functions for signing (and 
 validating) dictionaries and URLs.
 
+## Prerequisites
+
+```shell
+sudo dnf install php php-json
+```
+
 ## Installation
 
 ```shell
--- src/Placeholder.php
+++ src/Placeholder.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace barseghyanartur\ska;
+//namespace barseghyanartur\ska;
 
 /**
  * Signature lifetime in seconds.
@@ -29,35 +29,43 @@ const DEFAULT_VALID_UNTIL_PARAM = "valid_until";
  */
 const DEFAULT_EXTRA_PARAM = "extra";
 
-// Note this method returns a boolean and not the array
-function dictToOrderedDict(&$array): bool
+/**
+ * Dict to ordered dict.
+ *
+ * @param array $dict
+ * @return array
+ */
+function dictToOrderedDict(array $dict): array
 {
-    foreach ($array as &$value) {
+    foreach ($dict as &$value) {
         if (is_array($value)) dictToOrderedDict($value);
     }
-    return ksort($array);
+    ksort($dict);
+    return $dict;
 }
 
 /**
  * Sorted urlencode.
  *
- * @param {Object} data
- * @param {boolean} quoted
- * @returns {string}
+ * @param array $data
+ * @param bool $quoted
+ * @return string
  */
-function sortedURLEncode($data, $quoted = true) {
-    var $orderedData = dictToOrderedDict(data);
-    var $_sorted = [];
-    for (const [$key, $value] of Object.entries($orderedData)) {
-        if (isObject($value) || Array->isArray($value)) {
-            $_sorted.push(`${key}=${JSON.stringify($value)}`);
+function sortedURLEncode(array $data, bool $quoted = true): string
+{
+    $orderedData = dictToOrderedDict($data);
+    $_sorted = [];
+    foreach ( $orderedData as $key => $value) {
+        if (is_array($value)) {
+            $_sorted[] = "$key={${json_encode($value)}}";
+//            $_sorted[] = "$key={${json_encode($value)}}";
         } else {
-            $_sorted.push(`${key}=${value}`);
+            $_sorted[] = "${key}=${value}";
         }
     }
-    var $_res = _sorted.join("&");
+    $_res = implode("&", $_sorted);
     if ($quoted) {
-        $_res = encodeURIComponent(_res);
+        $_res = urlencode(_res);
     }
     return $_res;
 }
@@ -65,12 +73,12 @@ function sortedURLEncode($data, $quoted = true) {
 /**
  * Make a validUntil.
  *
- * @param {number} lifetime
- * @returns {string}
+ * @param int $lifetime
+ * @return string
  */
-function makeValidUntil($lifetime=SIGNATURE_LIFETIME): string
+function makeValidUntil(int $lifetime=SIGNATURE_LIFETIME): string
 {
-    var $validUntil = new Date();
+    $validUntil = new Date();
     $validUntil.setSeconds($validUntil.getSeconds() + $lifetime);
     return ($validUntil / 1000).toFixed(1);
 }
@@ -78,33 +86,32 @@ function makeValidUntil($lifetime=SIGNATURE_LIFETIME): string
 /**
  * Get sorted keys from dictionary given.
  *
- * @param {Object} dict
- * @param {boolean} returnString
- * @returns {string|string[]}
+ * @param array dict
+ * @param bool returnString
+ * @return string|string[]
  */
-function dictKeys($dict, $returnString = false): string
+function dictKeys(array $dict, bool $returnString = false): string
 {
-    var $keys = Object.keys($dict);
-    $keys.ksort();
+    $keys = array_keys($dict);
+    sort($keys);
     if ($returnString) {
-        return $keys.join(",");
+        return implode(",", $keys);
     }
     return $keys;
 }
 
-
 /**
  * Filters out non-white-listed items from the ``extra`` array given.
  *
- * @param {Object} data
- * @param {Array} extra
- * @returns {Object}
+ * @param array $data
+ * @param array $extra
+ * @returns array
  */
-function extractSignedData($data, $extra) {
-    var $dataCopy = JSON.parse(JSON.stringify($data));
-    for (const [$key, $value] of Object.entries($dataCopy)) {
+function extractSignedData(array $data, array $extra): array {
+    $dataCopy = unserialize(serialize($data));
+    foreach ($dataCopy as $key=> $value) {
         if ($extra.indexOf(key) < 0) {
-            delete $dataCopy[$key];
+            unset($dataCopy[$key]);
         }
     }
     return $dataCopy;
@@ -125,3 +132,96 @@ final class Placeholder
         return $this->prefix.$value;
     }
 }
+
+
+/**
+ * Sample payload
+ */
+$json = <<<EOD
+{
+    "order_lines": [{
+        "quantity": 4,
+        "product_id": "8273401260171",
+        "product_name": "himself",
+        "product_description": "Man movement another skill draw great late.",
+        "product_price_excl_tax": 7685,
+        "product_price_incl_tax": 8684,
+        "product_tax_rate_percentage": 13
+    }, {
+        "quantity": 5,
+        "product_id": "6760122207575",
+        "product_name": "someone",
+        "product_description": "Including couple happen ago hotel son know list.",
+        "product_price_excl_tax": 19293,
+        "product_price_incl_tax": 20064,
+        "product_tax_rate_percentage": 4
+    }, {
+        "quantity": 1,
+        "product_id": "5014352615527",
+        "product_name": "able",
+        "product_description": "Simply reason bring manager with lot.",
+        "product_price_excl_tax": 39538,
+        "product_price_incl_tax": 41910,
+        "product_tax_rate_percentage": 6
+    }, {
+        "quantity": 1,
+        "product_id": "4666517682328",
+        "product_name": "person",
+        "product_description": "Arrive government such arm conference program every.",
+        "product_price_excl_tax": 18794,
+        "product_price_incl_tax": 18794,
+        "product_tax_rate_percentage": 0
+    }, {
+        "quantity": 2,
+        "product_id": "3428396033957",
+        "product_name": "chance",
+        "product_description": "Ever campaign next store far stop and.",
+        "product_price_excl_tax": 26894,
+        "product_price_incl_tax": 29314,
+        "product_tax_rate_percentage": 9
+    }, {
+        "quantity": 4,
+        "product_id": "4822589619741",
+        "product_name": "style",
+        "product_description": "Song any season pick box chance.",
+        "product_price_excl_tax": 17037,
+        "product_price_incl_tax": 19422,
+        "product_tax_rate_percentage": 14
+    }],
+    "webshop_id": "4381a041-11cd-43fa-9fb4-c558bac1bd5e",
+    "order_id": "lTAGlTOHtKiBdvRvmhSw",
+    "amount": 491605,
+    "currency": "EUR",
+    "company": {
+        "name": "Siemens",
+        "registration_number": "LhkvLTWNTVNxlMKfBruq",
+        "vat_number": "RNQfPcPtnbDFvQRbJeNJ",
+        "website": "https://www.nedschroef.com/",
+        "country": "NL"
+    },
+    "user": {
+        "first_name": "Noor",
+        "last_name": "van Praagh",
+        "email": "juliegoyaerts-van-waderle@gmail.com",
+        "phone_number": "+31475013353"
+    },
+    "shipping": {
+        "street": "Femkeboulevard",
+        "house_number": "7",
+        "city": "Noord-Sleen",
+        "postal_code": "1784KL",
+        "country": "NL"
+    },
+    "billing": {
+        "street": "Pippasteeg",
+        "house_number": "35",
+        "city": "Ospel",
+        "postal_code": "6385 VA",
+        "country": "NL"
+    }
+}
+EOD;
+
+$payload = json_decode($json, true);
+
+print_r($payload);
